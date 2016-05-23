55d71314cb0c04a3e9e8c1fd8f6f6c7eb371a065
Patrick Burls <patrickburls@hotmail.com>
2016-05-23 10:08:03 +0100

    create new Dewey Manifest project.

---
+++ Dewey/Manifest/Repositories/LoadRepositoriesManifestResult.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Dewey.Manifest.Repositories
+{
+    public class LoadRepositoriesManifestResult
+    {
+        public FileInfo RepositoriesManifestFile { get; private set; }
+
+        public RepositoriesManifest RepositoriesManifest { get; private set; }
+
+        public IEnumerable<LoadRepositoryElementResult> LoadRepositoryElementResults { get; private set; }
+
+        private IEnumerable<string> _errorMessages;
+        public IEnumerable<string> ErrorMessages
+        {
+            get
+            {
+                if (_errorMessages == null)
+                {
+                    _errorMessages = GetErrorMessages();
+                }
+
+                return _errorMessages;
+            }
+        }
+
+        private LoadRepositoriesManifestResult()
+        {
+
+        }
+
+        public static LoadRepositoriesManifestResult CreateFileNotFoundResult(FileInfo repositoriesManifestFile)
+        {
+            var result = new LoadRepositoriesManifestResult();
+            result.RepositoriesManifestFile = repositoriesManifestFile;
+            return result;
+        }
+
+        public static LoadRepositoriesManifestResult CreateSuccessfulResult(FileInfo repositoriesManifestFile, RepositoriesManifest repositoriesManifest, IEnumerable<LoadRepositoryElementResult> loadRepositoryElementResult)
+        {
+            var result = new LoadRepositoriesManifestResult();
+            result.RepositoriesManifestFile = repositoriesManifestFile;
+            result.RepositoriesManifest = repositoriesManifest;
+            result.LoadRepositoryElementResults = loadRepositoryElementResult;
+            return result;
+        }
+
+        private IEnumerable<string> GetErrorMessages()
+        {
+            if (!RepositoriesManifestFile.Exists) return new string[] { string.Format("Manifest file '{0}' not found.", RepositoriesManifestFile.FullName) };
+
+            return LoadRepositoryElementResults.Where(x => x.ErrorMessage != null).Select(x => x.ErrorMessage);
+        }
+    }
+}
---
+++ Dewey/Manifest/Repository/LoadRepositoryItemResult.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Dewey.Manifest.Repository
+{
+    public class LoadRepositoryItemResult
+    {
+        public DirectoryInfo RepositoryDirectory { get; private set; }
+
+        public FileInfo RepositoryManifestFile { get; private set; }
+
+        public RepositoryManifest RepositoryManifest { get; private set; }
+
+        public IEnumerable<LoadComponentElementResult> LoadComponentElementResults { get; private set; }
+
+        private IEnumerable<string> _errorMessages;
+        public IEnumerable<string> ErrorMessages
+        {
+            get
+            {
+                if (_errorMessages == null)
+                {
+                    _errorMessages = GetErrorMessages();
+                }
+
+                return _errorMessages;
+            }
+        }
+
+        private LoadRepositoryItemResult() { }
+
+        public static LoadRepositoryItemResult CreateDirectoryNotFoundResult(DirectoryInfo repositoryDirectory)
+        {
+            var result = new LoadRepositoryItemResult();
+            result.RepositoryDirectory = repositoryDirectory;
+            return result;
+        }
+
+        public static LoadRepositoryItemResult CreateFileNotFoundResult(DirectoryInfo repositoryDirectory, FileInfo repositoryManifestFile)
+        {
+            var result = new LoadRepositoryItemResult();
+            result.RepositoryDirectory = repositoryDirectory;
+            result.RepositoryManifestFile = repositoryManifestFile;
+            return result;
+        }
+
+        public static LoadRepositoryItemResult CreateSuccessfulResult(DirectoryInfo repositoryDirectory, FileInfo repositoryManifestFile, RepositoryManifest repositoryManifest, IEnumerable<LoadComponentElementResult> loadComponentElementResults)
+        {
+            var result = new LoadRepositoryItemResult();
+            result.RepositoryDirectory = repositoryDirectory;
+            result.RepositoryManifestFile = repositoryManifestFile;
+            result.RepositoryManifest = repositoryManifest;
+            result.LoadComponentElementResults = loadComponentElementResults;
+            return result;
+        }
+
+        private IEnumerable<string> GetErrorMessages()
+        {
+            if (!RepositoryDirectory.Exists) return new string[] { string.Format("Respository directory '{0}' not found.", RepositoryDirectory.FullName) };
+            if (!RepositoryManifestFile.Exists) return new string[] { string.Format("Repository Manifest file '{0}' not found.", RepositoryManifestFile.FullName) };
+
+            return LoadComponentElementResults.Where(x => x.ErrorMessage != null).Select(x => x.ErrorMessage);
+        }
+    }
+}
