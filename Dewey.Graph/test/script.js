2a341a3240e67f8fafe34f3c3fe198e809758a07
Patrick Burls <pburls@gmail.com>
2016-10-05 22:56:52 +0100

    add test dependency graph html and javascript.

---
+++ Dewey.Graph/test/script.js
@@ -0,0 +1,16 @@
+const width = 600;
+const height = 400;
+
+const g = new Dracula.Graph
+
+g.addEdge('Banana', 'Tomato')
+g.addEdge('Mushroom', 'Tomato')
+g.addEdge('Apple', 'Tomato')
+g.addEdge('Apple', 'Mushroom')
+g.addEdge('Banana', 'Mushroom')
+
+const layouter = new Dracula.Layout.Spring(g)
+
+const renderer = new Dracula.Renderer.Raphael('#canvas', g, width, height)
+
+renderer.draw()
