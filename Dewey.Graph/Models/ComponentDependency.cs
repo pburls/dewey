f6c7f92472ae68bfadc51b847170ac031e290fde
Patrick Burls <pburls@gmail.com>
2017-03-30 21:33:53 +0100

    changed graph to use new dewey json manifests

---
+++ Dewey.Graph/Models/ComponentDependency.cs
@@ -0,0 +1,18 @@
+﻿using Dewey.Manifest.Models;
+using Newtonsoft.Json.Linq;
+
+namespace Dewey.Graph.Models
+{
+    public class ComponentDependency : Dependency
+    {
+        public const string COMPONENT_DEPENDENCY_TYPE = "component";
+
+        public string protocol { get { return (string)BackingData["protocol"]; } set { BackingData["protocol"] = value; } }
+
+        public ComponentDependency() { }
+
+        public ComponentDependency(Dependency dependancy) : base(dependancy.BackingData) { }
+
+        public ComponentDependency(JObject data) : base(data) { }
+    }
+}
